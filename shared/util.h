0840a8a64c8c111e401716b62114e9b2beef5af1
Josh Trew <trewtzu@gmail.com>
2013-09-16 16:35:39 +1000

    fix for error on last commit

---
+++ shared/util.h
@@ -0,0 +1,18 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <netdb.h> 
+
+
+
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+
+#endif
