52d8a814f546d6ad6e1cc9c40cf63553712e30ae
Josh Trew <trewtzu@gmail.com>
2013-09-16 13:46:08 +1000

    Made more readable as an example S/C setup

--- client/client.c
+++ client/client.c
@@ -1,3 +1,12 @@
+/*
+ * Example client,  for the JAC RaspberryPi Jukebox system
+ *
+ *
+ *
+ *
+ * initial design heavy based on  http://www.linuxhowtos.org/C_C++/socket.htm
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +16,11 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+
+/*
+ * Simple error output, 'msg' should be the conext for the error
+ */
 void error(const char *msg)
 {
     perror(msg);
@@ -24,34 +38,60 @@ int main(int argc, char *argv[])
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+    
+    //grab the port
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    
+	//grab the ip
+	server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+    
+    //Prep the connection rules
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
+    //NOTE: this is deprecated. ill get rid of it when i put a real header in and replace it with a macro
+	bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
+	
+	//and connect
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
+    
+	/*
+	 * At this point client behaviour starts
+	 */
+	
+	printf("Starting client payload");
+	int i=0;
+	while(i<20){
+		//printf("Please enter the message: ");
+		//bzero(buffer,256);
+		//fgets(buffer,255,stdin);
+	
+		sprintf(buffer, "%d", i);
+		n = write(sockfd,buffer,strlen(buffer));
+	
+		if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
+		bzero(buffer,256);
+		n = read(sockfd,buffer,255);
+		if (n < 0) 
+			error("ERROR reading from socket");
     printf("%s\n",buffer);
-    close(sockfd);
+	
+	sleep(3);
+	i++;	
+	
+	}
+	
+	close(sockfd);
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -4,9 +4,13 @@
  *
  *
  *
- * http://www.linuxhowtos.org/C_C++/socket.htm
-*/
+ * initial design heavy based on  http://www.linuxhowtos.org/C_C++/socket.htm
+ */
 
+
+/*
+ * NOTES: need to add code to close socket before all mode of exit as i think it takes the network card a while to do it on its own.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,31 +19,40 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
 
+//needs to be put in a header or just put in line, but i think i might be useful later
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+void *clientAction(int sock, int j);
+
+/*
+ * Simple error output, 'msg' should be the conext for the error
+ */
 void error(const char *msg)
 {
 	perror(msg);
 	exit(1);
 }
 
+/*
+ * simple usage disply
+ */
 void usage()
 {
 	fprintf(stdout,"Usage: server portNum");	
 }
 
+
 int main(int argc, char *argv[])
 {
 	int sockfd, newsockfd, portNum;
 	socklen_t clilen;
-	char buffer[256];
 
 	struct sockaddr_in serv_addr, cli_addr;
 	int n;
 
 	if (argc < 2) {
 		fprintf(stderr,"ERROR, no port provided\n");
-		usage()
+		usage();
 		exit(1);
 	}
 
@@ -52,27 +65,78 @@ int main(int argc, char *argv[])
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	portNum = atoi(argv[1]);
 
-
+	//prep sock settings
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY; // INADDR_ANY  is this mechines address
 	serv_addr.sin_port = htons(portNum); //note htons ensures the int is in network btye order
-	//i dont trustthat cast
+	
+
+	//put the socket in to action
+	//
+	//this needs to be checked over, i dont like that pointer-cast-address
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
 		error("ERROR on binding");
-	listen(sockfd,5);
-	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, 
-			(struct sockaddr *) &cli_addr, 
-			&clilen);
-	if (newsockfd < 0) 
-		error("ERROR on accept");
-	bzero(buffer,256);
-	n = read(newsockfd,buffer,255);
-	if (n < 0) error("ERROR reading from socket");
-	printf("Here is the message: %s\n",buffer);
-	n = write(newsockfd,"I got your message",18);
-	if (n < 0) error("ERROR writing to socket");
-	close(newsockfd);
+
+	if(listen(sockfd,5) == -1)
+		error("ERROR on listen");
+
+
+	printf("\nServer is starting on port: %d\n",portNum);
+	
+	/*
+	 * Below is the threading loop, this needs to be modded to suit, but its pretty stright forward
+	 * */
+
+	int j =0;
+	while(j<10)
+	{
+	
+		j++;
+		printf("in loop, @ %di\n", j);
+		
+		//i have no idea at all what cli_addr is used for
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen); //this is the core action of connection C to S
+		if (newsockfd < 0) 
+			error("ERROR on accept");
+		
+
+		printf("forking\n");
+		pthread_t thread; //note, if we want to access the thread later we will need to store this somewhere
+		pthread_create(&thread,NULL,&clientAction,newsockfd,j);	 //this core action of starting a thread, arg 3 is the func name it will start on, all past that are args to pass to that func
+		printf("forked");
+	}
+
+	//close the socket
 	close(sockfd);
 	return 0;
-} 
+}
+
+
+/*
+ * This is an example action to be take by our thread, 
+ */
+void *clientAction(int clisock, int id){
+	int n;
+	printf("starting action\n");
+	char buffer[256];
+
+
+	while(1){
+		bzero(buffer,sizeof(buffer));
+		n = read(clisock,buffer,255);
+		
+		if (n < 0)
+			error("ERROR reading from socket");
+		
+		printf("Client %d:: %s\n",id,buffer);
+		
+		n = write(clisock,"Ack",18);
+		if (n < 0)
+			error("ERROR writing to socket");
+
+	}
+	
+	printf("ending");
+	close(clisock);
+}
