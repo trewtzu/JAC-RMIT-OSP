57fddfcef8f9463152bde80e9e3e01d4c97a3833
Cory Mollison <s3369723@student.rmit.edu.au>
2013-10-05 03:48:17 +1000

    Removed reliance on queue.c Client code no longer needs access to the specific server side code such as the queue structure or fileio functions.

--- client/selectionController.c
+++ client/selectionController.c
@@ -4,65 +4,75 @@
 void begin(int simple, int sockfd){
 
 	int n;
-		char buffer[PACKET_S+1];
+	char buffer[PACKET_S+1];
 		
 	//NOTE: Simeple interface is blocking!
 	if(simple==1){
 		while(1){
-			queue_t *queue;
-			queue_t *listing; 
-			queue = new_queue ();
-			listing = new_queue ();
+  
+    char **allsongs;
+    char **qsongs;
+    int song_count;
+    int q_count; 
+    int i;
 
-				n = write(sockfd,REQUEST_LISTS, PACKET_S);
+				n = write(sockfd, REQUEST_LISTS, PACKET_S);
 
 				if (n < 0) 
 					error("ERROR writing to socket");
 
-				bzero(buffer,PACKET_S+1);
+				bzero(buffer, PACKET_S+1);
+        
+        /* Get song count from server */
+        n = read(sockfd, &song_count, PACKET_S);
+			  if (n < 0) 
+					error("ERROR read from socket");
+        
+        allsongs = safe_malloc ((song_count + 1) * sizeof (char *));
 
-//			printf("Track List:\n");
-				while(1){
+				i = 0;
+        while(i < song_count){
+
+					n = read(sockfd, buffer, PACKET_S);
+					if (n < 0) 
+						error("ERROR read from socket");
+				  
+          allsongs[i] = safe_strdup (buffer);
+          i++;
+        }
+        /* NULL terminate array */ 
+        allsongs[i] = NULL;
+        
+        /* Get queue count from server */
+        n = read(sockfd, &q_count, PACKET_S);
+			  if (n < 0) 
+					error("ERROR read from socket");
+        
+        qsongs = safe_malloc ((q_count + 1) * sizeof (char *));
+        
+        i = 0;
+
+				while(i < q_count){
 
 					n = read(sockfd,buffer,PACKET_S);
 					if (n < 0) 
 						error("ERROR read from socket");
-					//TODO: this will need to be replaced by a const proto code
-					if( strcmp(buffer,END_OF_TRACKS) == 0){
-//					printf("Done\n");
-						break;
-					}
-					add_song(listing, buffer);
-//				printf("%s\n",buffer);
-				}	
+				  
+          qsongs[i] = safe_strdup (buffer);
+          i++;
+        }
+        /* NULL terminate array */
+        qsongs[i] = NULL;
 
-//			printf("Queue List:\n");
-				while(1){
-
-					n = read(sockfd,buffer,PACKET_S);
-					if (n < 0) 
-						error("ERROR read from socket");
-					//TODO: this will need to be replaced by a const proto code
-					if( strcmp(buffer,END_OF_QUEUE) == 0){
-//					printf("Done\n");
-						break;
-					}
-					add_song(queue, buffer);
-//			  printf("%s\n",buffer);
-				}
-
-
-
-				//TODO: proto code here
 				n = write(sockfd,"ack",PACKET_S);
 
 				if (n < 0) 
 					error("ERROR writing to socket");
 
-				simpleDisplay( get_list( listing), listing->count, get_list( queue), queue->count);
-				
-				//TODO remove magic numbers
-				bzero(buffer, PACKET_S+1);
+				simpleDisplay(allsongs, qsongs);
+			  
+        /* Manage user interaction / menu here */
+        bzero(buffer, PACKET_S+1);
 				fgets(buffer, PACKET_S, stdin);
 				if(buffer == NULL || buffer[0] =='\n') {
 					printf("nul buf");
@@ -72,37 +82,40 @@ void begin(int simple, int sockfd){
 					n = write(sockfd,ADD_TO_QUEUE,PACKET_S);
 					if (n < 0) 
 						error("ERROR writing to socket");
-					
-					
-				
-				//TODO this should be less memory shit
-				delete_queue(queue);
-				delete_queue(listing);
-			
-			}
-		}
+        }
+      free (allsongs);
+      free (qsongs);
+      
+      }
 		
-	}
-	else{
-		printf("not ready for non simple UI");
-	}
-	}
+	  }
+	  else
+    {
+		  printf("not ready for non simple UI");
+	  }
+  }
 
-	void simpleDisplay(char** trackList, int trackList_s, char** trackQueue, int trackQueue_s){
+	void simpleDisplay(char** list, char** queue){
 
 		int i=0;
-		int j=0;
-    
+    int j=0;
+
     print_logo ();
 
 		printf("|%32s || %s%26s\n", "Queue", "Songs", "|");
-		for(i=0; i<trackList_s; i++){
-			if(i<trackQueue_s)
-				printf("|%d: %-30s||",i+1,trackQueue[i]);
-			else
+		
+    while (list[i] != NULL)
+    {
+			if(queue[j] != NULL)
+      {
+				printf("|%d: %-30s||",j+1,queue[j]);
+			  j++;
+      }
+      else
 				printf("|%-33s||"," ");
-			printf("%d%30s|\n",i+1,trackList[i]);
 
+			printf("%d%30s|\n",i+1,list[i]);
+      i++;
 		}
 	}
 
